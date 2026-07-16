e3ffe7cc88caa93fd55ead634857e5107f5cc7dc
MojiNoSoCute <watcharakorn820@gmail.com>
2026-07-16 15:30:20 +0700

    add db

--- db.js
+++ db.js
@@ -1,2 +1,30 @@
 // import Sequelize and DataTypes
 import { Sequelize, DataTypes } from "sequelize";
+
+// DB connection
+// db_name , user, password, { configulation option }
+const sequelize = new Sequelize("postgres_db", "dev_user", "dev_password", {
+  host: "localhost",
+  port: 5432,
+  dialect: "prostgres",
+  loggin: false,
+});
+
+// define databse schema
+// จากตาราง Product และต้องกำหนดทุกอันให้ถูก
+const Product = sequelize.define("Product", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    }
+})
